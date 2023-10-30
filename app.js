e58b9022ffad547f9d9b572a64e63e9b71726d4d
Lacatis, Danuta Tina <danutatina.lacatis@iso-gruppe.com>
2023-10-30 10:48:37 +0100

    add more modifiers to change the name only on hitting enter key

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmedName: "",
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
     submitForm(event) {
       alert("Submitted!");
     },
